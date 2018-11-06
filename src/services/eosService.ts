9936edd3847650fa8f9a3e7c339bf11a237e21c5
Yuriy Plotnikov <yuriy.plotnikov@outlook.com>
2018-11-06 14:01:36 +0300

    Revert "Trying to figure out the reason of slow balance recording"
    This reverts commit 244da7aea325f4cf3c790e9abdcb6d8989a6de80.

--- src/services/eosService.ts
+++ src/services/eosService.ts
@@ -89,8 +89,6 @@ export class EosService {
                     const operationId = await this.operationRepository.getOperationIdByTxId(txId);
                     if (!!operationId) {
 
-                        await this.log(LogLevel.info, "Operation detected", operationId);
-
                         // this is our operation, so use our data 
                         // to record balance changes and history
 
@@ -117,8 +115,6 @@ export class EosService {
 
                         // set operation state to completed
                         await this.operationRepository.update(operationId, { completionTime: new Date(), blockTime, block });
-
-                        await this.log(LogLevel.info, "Operation recorded", operationId);
                     } else {
 
                         // this is external transaction, so use blockchain 
